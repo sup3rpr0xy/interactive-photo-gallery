5018fcb1d7d443589147bb4cbac2cf20179bdde9
Alfred Thomas Torres <300awoo@gmail.com>
2016-04-10 16:36:09 +0800

    added the search functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-  $('.fancybox').fancybox();  
+  $('.fancybox').fancybox();
+  
+  $('.live-search-list li').each(function(){
+    $(this).attr('data-search-term', $(this).text().toLowerCase());
+  });
+
+  $('.live-search-box').on('keyup', function(){
+
+  var searchTerm = $(this).val().toLowerCase();
+
+      $('.live-search-list li').each(function(){
+
+          if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+              $(this).show();
+          } else {
+              $(this).hide();
+          }
+
+      });
+
+  });
+
 });
